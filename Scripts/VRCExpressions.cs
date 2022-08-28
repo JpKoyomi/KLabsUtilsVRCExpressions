5c0e62c103e6df9a26716a5b360f8d024ca05564
Koyomi <fox@9ts.jp>
2022-08-28 22:54:40 +0900

    メインアセット用ダミー

---
+++ Scripts/VRCExpressions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+namespace KLabs.VRC.Extentions
+{
+    public class VRCExpressions : ScriptableObject
+    {
+        [field: SerializeField]
+        public VRCExpressionParameters Parameters { get; set; }
+
+        [field: SerializeField]
+        public VRCExpressionsMenu[] Menu { get; set; }
+    }
+}
